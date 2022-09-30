049442434538fd9b908f85aced6e875a60084dff
pauline <paulineparmigiani@gmail.com>
2022-09-30 14:01:18 -0700

    prints size of various types

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 printf("Size of char: %zu bytes\n", sizeof(char));
 printf("Size of an int: %d bytes\n", sizeof(int));
 printf("Size of a long int: %ld bytes\n", sizeof(int));
-printf("Size of a long long int: %ld bytes\n", sizeof(int));
+printf("Size of a long long int: %lld bytes\n", sizeof(int));
 printf("Size of a float: %ld bytes\n", sizeof(float));
 return (0);
 }
