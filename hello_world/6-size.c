cf87cafe249b483fcada13e112bda8808bca581d
pauline <paulineparmigiani@gmail.com>
2022-09-30 13:16:13 -0700

    prints size of various types

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+# include < stdio.h >
 
 /**
  * main - Entry point
@@ -7,63 +7,10 @@
  */
 int main(void)
 {
-  printf("Size of char: % bytes\n" , sizeof(charType));
-  printf("Size of an int: %d bytes\n" , sizeof(intType));
-  printf("Size of a long int: %ld bytes\n" , sizeof(longintType));
-  printf("Size of a long long int: %lld bytes\n" , sizeof(longlongType));
-  printf("Size of a float: %f bytes\n" , sizeof(charType));
-      return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-d bytes\n",sizeof(integerType));
-    printf("Size of float: %ld bytes\n",sizeof(floatType));
-    printf("Size of double: %ld bytes\n",sizeof(doubleType));
-    printf("Size of char: %ld byte\n",sizeof(charType));
+printf("Size of char: % bytes\n", sizeof(charType));
+printf("Size of an int: %d bytes\n", sizeof(intType));
+printf("Size of a long int: %ld bytes\n", sizeof(longintType));
+printf("Size of a long long int: %lld bytes\n", sizeof(longlongType));
+printf("Size of a float: %f bytes\n", sizeof(charType));
 return (0);
 }
