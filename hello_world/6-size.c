406e89b6995f9ceb04cd0400a22cfa788d6b6821
pauline <paulineparmigiani@gmail.com>
2022-09-30 13:44:54 -0700

    prints size of various types

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,7 +5,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int main(int argc, char *argv[]) ;
 {
 printf("Size of char: % bytes\n", sizeof(charType));
 printf("Size of an int: %d bytes\n", sizeof(intType));
