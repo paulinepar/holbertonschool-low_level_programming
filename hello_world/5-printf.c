9db925d3827ff3a53a10e862b2ccddd14c549e2f
pauline <paulineparmigiani@gmail.com>
2022-09-30 10:46:48 -0700

    printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -2,12 +2,12 @@
 
 /**
  * main - Entry point
- *
+ * printf - Display the text
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,")
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
 
