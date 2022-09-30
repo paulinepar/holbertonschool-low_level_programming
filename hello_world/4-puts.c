473dda9a7ddd6a01a4ede0f37cc84efa3d52931a
pauline <paulineparmigiani@gmail.com>
2022-09-30 10:19:09 -0700

    write C programm

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -8,9 +8,8 @@
 int main(void)
 {
   
-char str1[] = "Programming is like building a multilingual puzzle,\";
+  puts("\"Programming is like building a multilingual puzzle,");
 
-puts(str1);
 
     return (0);
 }
