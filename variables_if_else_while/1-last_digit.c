3a1b8f178432bf98aec2e221d25e22f47f970e0c
pauline <paulineparmigiani@gmail.com>
2022-10-03 06:56:02 -0700

    variables else if 2

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -18,11 +18,9 @@ if (l > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 }
-else if (l > 0)
-printf("Last digit of %d is %d and not 0\n", n, n % 10);
-else if (l < 6)
+else if (l < 6 && l != 0)
 {
-printf("Last digit of %d is %d and is less than 6\n", n, n % 10);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 }
 else
 {
