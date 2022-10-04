14ef3a36387549f623ffe7286edec2f97c4d7bd6
pauline <paulineparmigiani@gmail.com>
2022-10-04 05:59:58 -0700

    hexadecimal

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -11,11 +11,11 @@ int main(void)
 {
 int num;
 int ch;
-
+{
 for (num = '0'; num <= '9'; num++)
 putchar(num);
 for (ch = 'a'; ch <= 'f'; ch++)
 putchar(ch);
-putchar("\n");
+putchar('\n');
 return (0);
 }
