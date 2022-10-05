6442406b16474fa7bcac84723bf1fec7818d1e36
pauline <paulineparmigiani@gmail.com>
2022-10-05 08:21:35 -0700

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void){
+  
+int al = 'a';
+while(al <= 'z')
+{
+_putchar(al);
+al++;
+}
+_putchar('\n');
+}
