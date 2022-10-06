f6693357961778e56de322fc576099b74aa7b6f2
pauline <paulineparmigiani@gmail.com>
2022-10-06 09:40:34 -0700

    upper and lower check

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isalpha - check character
+ * @c: check character
+ * Return: Always 0 (Success)
+ */
+
+int _isalpha(int c)
+{
+if (c > 'a' && c < 'z')
+return (1);
+else if (c > 'A' && c < 'Z')
+return (1);
+else
+return (0);
+}
