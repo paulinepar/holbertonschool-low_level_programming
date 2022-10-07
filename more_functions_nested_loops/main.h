c91ef3d9e85535778d08b89af775156eb6843a3d
pauline <paulineparmigiani@gmail.com>
2022-10-07 04:48:46 -0700

    readme

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef HEADER_H
+#define HEADER_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int _isupper(int c);
+
+#endif
