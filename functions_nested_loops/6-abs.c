7eed945947a54bfd9526a0611413a57778291d4b
pauline <paulineparmigiani@gmail.com>
2022-10-07 03:10:37 -0700

    computes absolute value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value
+ * @n: check character
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int n)
+{
+
+if (n < 0)
+{
+return (n * -1);
+}
+return (n);
+}
