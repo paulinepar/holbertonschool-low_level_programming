97e002b7de4985829c3b0b2b9a0cd12b99dfc8e7
pauline <paulineparmigiani@gmail.com>
2022-10-07 06:04:18 -0700

    prints numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ * 
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+int n;
+for (n = '0'; n >= '9'; n++)
+_putchar(n);
+return (0);
+}
