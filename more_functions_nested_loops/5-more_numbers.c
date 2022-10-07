e1e6e9b25acfffc73d1684828f71fd763ac7a0f2
pauline <paulineparmigiani@gmail.com>
2022-10-07 13:13:36 -0700

    print x10 numbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers
+ * Return: Always 0 (Success)
+ */
+
+void more_numbers(void)
+{
+int n;
+int i;
+
+for (i = 0; i < 10; i++)
+{
+for (n = 0; n <= 14; n++)
+{
+_putchar (n);
+}
+putchar ('\n');
+}
+}
