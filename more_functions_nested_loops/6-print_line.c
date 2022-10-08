22efef1a0f2ab3dd5a5b23f40f2a2c122903ed63
pauline <paulineparmigiani@gmail.com>
2022-10-08 08:09:36 -0700

    printslines

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - print numbers
+ * Return: Always 0 (Success)
+ */
+
+void print_line(int n)
+{
+if (n > 0)
+{
+_putchar ('_');
+}
+else
+{
+_putchar ('\n');
+}
+}
+
