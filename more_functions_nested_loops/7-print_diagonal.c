ea1e6cbb737010b9088535023aa416d3630811ab
pauline <paulineparmigiani@gmail.com>
2022-10-08 12:52:50 -0700

    print diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print lines
+ * @n: numbers
+ * Return: Always 0 (Success)
+ */
+
+void print_diagonal(int n)
+{
+int a;
+for (a = 0; a < n; a++)
+{
+_putchar ('\'');
+}
+_putchar('\n');
+}
