b34b8262b84b50912c5695a53ae87381fae794fc
paulinepar <paulineparmigiani@gmail.com>
2022-10-10 23:56:02 +0200

    returns length

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * strlen - returns the length
+ * _strlen - returns the length of a string
  * @s: int
  * Return: Always 0 (Success)
  */
@@ -9,7 +9,6 @@ int _strlen(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++);
-
-	return 0;
+	for (i = 0; s[i] != '\o'; i++)
+		return (i);
 }
