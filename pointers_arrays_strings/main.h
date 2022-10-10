d130bdf8e23adceb0f98478eb6d72b3edcf4f9ad
paulinepar <paulineparmigiani@gmail.com>
2022-10-10 21:25:12 +0200

    main of 0-reset to 98

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,18 +1,25 @@
-#include "main.h"
+#ifndef HEADER_H
+#define HEADER_H
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-/**
- * main - check the code 
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int n;
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_square(int size);
+void print_diagonal(int n);
+void reset_to_98(int *n);
 
-	n = 402;
-	printf("n=%d\n", n);
-	reset_to_98(&n);
-	printf("n=%d\n", n);
-	return (0);
-}
+#endif
