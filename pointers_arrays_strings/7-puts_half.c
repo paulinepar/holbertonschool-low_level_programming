44fa682b92170042257af1661e2bfb1373d1968a
paulinepar <paulineparmigiani@gmail.com>
2022-10-11 22:25:27 +0200

    prints string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - print a string
+ * @str: char
+ * Return: (i)
+ */
+
+void puts_half(char *str)
+{
+	int n;
+	int l = 0;
+
+	for (n = 0; str[n] != '\0'; n++)
+	{
+		l++;
+	}
+	l = l / 2;
+	for (; str[l] != '\0'; l++)
+	{
+		_putchar(str[l]);
+	}
+}
