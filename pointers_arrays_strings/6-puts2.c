219676efd7140ce1845553483fcc93d8357c6c5b
paulinepar <paulineparmigiani@gmail.com>
2022-10-11 19:36:46 +0200

    print character string

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string
+ * @s: char
+ * Return: (i)
+ */
+void puts2(char *str)
+{
+	int h;
+	int l = 0;
+
+	for (h = 0; str[h] != '\0'; h++)
+	{
+		l++;
+	}
+	for (h = 0; h <= l; h += 2)
+	{
+		_putchar(str[h]);
+	}
+	_putchar('\n');
+}
