672088de7d51bcb12a338e7c1670685455557ae2
paulinepar <paulineparmigiani@gmail.com>
2022-10-14 20:14:41 +0200

    encodes

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strncpy - concatene two string
+ * leet - encodes a string
  * @s: char
  * Return: (i)
  */
@@ -11,9 +11,9 @@ char *leet(char *s)
 	char a[] = "aAeEoOtTlL";
 	char b[] = "4433007711";
 
-	for ( i = 0; s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for ( j = 0; j <= 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			if (a[j] == s[i])
 				s[i] = b[j];
