ab355f757ac9f8bba70f27d009b9dd65fc144a51
paulinepar <paulineparmigiani@gmail.com>
2022-10-14 15:05:37 +0200

    capitalizes all word

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -9,16 +9,15 @@
 char *cap_string(char *n)
 {
 	int i, x;
-	char ca[] = {',', ';', '.', '!', '?', '"',
-		     ')', '(', '{', '}', ' ', '\t', '\n'};
+	char ca[] = "\n\t,' ';.!\"?(){}";
 
 	for (i = 0; n[i] != '\0'; i++)
 	{
-		for (x = 0; ca[x] != 0; x++)
+		for (x = 0; ca[x] != 0 ; x++)
 		{
 			if (n[i] == ca[x] && n[i + 1] != '\0')
+				if (n[i + 1] >= 'a' && n[i + 1] <= 'z')
 				n[i + 1] -= 32;
-			break;
 		}
 	}
 	return (n);
