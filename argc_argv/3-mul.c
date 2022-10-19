dfd1be1948ae4f8aa2ec5df8fac5bc0868e2778d
paulinepar <paulineparmigiani@gmail.com>
2022-10-19 11:49:41 +0200

    multiplies

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - argc argv
+ * @argc: char
+ * @argv: char
+ * Return: (i)
+ */
+
+int main(int argc, char *argv[])
+{
+	int n, i;
+	int res;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+        n = atoi(argv[1]);
+	i = atoi(argv[2]);
+	res = n * i;
+		printf("%d\n", res);
+	}
+	return (0);
+}
