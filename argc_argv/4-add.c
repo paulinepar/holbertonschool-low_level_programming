5761442519e29bda86f6f31be82cc636b8fd7145
paulinepar <paulineparmigiani@gmail.com>
2022-10-19 16:32:16 +0200

    add numbers

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - argc argv
@@ -6,3 +8,39 @@
  * @argv: char
  * Return: (i)
  */
+
+int main(int argc, char *argv[])
+{
+
+	int n, i;
+	int res = 0;
+	int a;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (n = 1; n < argc; n++)
+	{
+		for (i = 0; argv[n][i] != '\0'; i++)
+		{
+			if (argv[n][i] >= 48 && argv[n][i] <= 57)
+			{
+				continue;
+			}
+			else
+			{
+				printf("Error\n");
+					return (1);
+			}
+		}
+	}
+	for (n = 1; n < argc; n++)
+	{
+		a = atoi(argv[n]);
+		res = res + a;
+	}
+	printf("%d\n", res);
+	return (0);
+}
