c2f0ce1c75f725ddfe512d28fc895d072b68b838
paulinepar <paulineparmigiani@gmail.com>
2022-10-24 13:44:49 +0200

    alloced memory

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - create array
+ * @c: char
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i = 0;
+
+	if (size == 0)
+		return (NULL);
+
+	array = malloc(sizeof(char) * size);
+	if (array ==  NULL)
+		return (NULL);
+
+
+	for (i = 0; i < size; i++)
+		array[i] = c;
+
+	return (array);
+}
