a4ea401f9ca1144aa12f84832559e8aae8bd72bf
paulinepar <paulineparmigiani@gmail.com>
2022-10-31 13:40:14 +0100

    initialize

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - initialize variable
+ * @name:
+ * @age:
+ * @owner:
+ * Return: Always 0.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	d->age = age;
+	d->owner = owner;
+}
