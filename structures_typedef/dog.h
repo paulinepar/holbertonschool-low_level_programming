934756419d2647a4c89bb6ea14dada15c2308505
paulinepar <paulineparmigiani@gmail.com>
2022-10-31 12:10:42 +0100

    define struct

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef _DOG
 #define _DOG
 
+/**
+ * struct dog - define new type
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+
 struct dog
 {
 	char *name;
