9e8ecb910aec49ffaae09fcaf371a1f1dae508bd
paulinepar <paulineparmigiani@gmail.com>
2022-11-02 10:17:20 +0100

    intindex

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * int_index - entry point
+ * @array: array
+ * @size: size of array
+ * @cmp: pointer of function
+ * Return: 0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	if (array == NULL && cmp == NULL)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		cmp(array[i]);
+		return (i);
+	}
+	return (-1);
+}
