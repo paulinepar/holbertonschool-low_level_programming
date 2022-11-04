ec90572f6d173c4ffdbd983c73dcc4a7744b10e2
paulinepar <paulineparmigiani@gmail.com>
2022-11-04 14:35:27 +0100

    free

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * new_dog - create new dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Return: return pointer
+ */
+
+void free_dog(dog_t *d)
+{
+	free(a);
+	free(a->name);
+	free(a->owner);
+}
