6b7a23adcb73e556c84f7a1d01384c1f709eddd1
paulinepar <paulineparmigiani@gmail.com>
2022-11-05 16:32:50 +0100

    print numbers

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - return the sum of all
+ * @n: First member
+ *
+ * Return: result
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	if (n == 0)
+		return;
+
+	if (separator == NULL)
+		return;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (separator != NULL)
+		{
+		printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(args);
+
+}
