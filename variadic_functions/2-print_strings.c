6c7e1643c6d283c594612beed4b1915baad6f636
paulinepar <paulineparmigiani@gmail.com>
2022-11-05 17:34:47 +0100

    up

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -28,7 +28,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("(nil)");
 		}
-		if (separator != NULL && i < n - 1)
+		if (separator != NULL && i != n - 1)
 		{
 			printf("%s", separator);
 		}
