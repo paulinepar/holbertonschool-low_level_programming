b4836304eb4070193aaef31df7e524befb607a14
paulinepar <paulineparmigiani@gmail.com>
2022-11-05 16:48:39 +0100

    print numbers

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -17,9 +17,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	if (n == 0)
 		return;
 
-	if (separator == NULL)
-		return;
-
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
