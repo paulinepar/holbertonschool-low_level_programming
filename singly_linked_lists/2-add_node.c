53d8ef787ad500119ac9c5ac153df548116c4159
paulinepar <paulineparmigiani@gmail.com>
2022-11-15 13:45:23 +0100

    returns the number of elements

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+#include "lists.h"
+#include <string.h>
+
+/**
+ * add_node - add node
+ * @head:
+ * @str: const char point
+ * Return: d
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *p = malloc(sizeof(list_t));
+
+	if (p == NULL)
+		return (NULL);
+
+	p->str = strdup(str);
+	p->len = strlen(str);
+	p->next = *head;
+	*head = p;
+
+	return (p);
+}
