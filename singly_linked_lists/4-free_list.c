3471644b994345ddf4fbeb6432f02bcedc55f51f
paulinepar <paulineparmigiani@gmail.com>
2022-11-15 21:07:04 +0100

    free list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+#include "lists.h"
+#include <string.h>
+
+/**
+ * free_list - free list_t
+ *
+ *
+ * Return: 0
+ */
+
+void free_list(list_t *head)
+{
+	if (head == NULL)
+		return;
+
+	if (head->next != NULL)
+	{
+		free(head->next);
+		free(head->str);
+		free(head);
+	}
+}
