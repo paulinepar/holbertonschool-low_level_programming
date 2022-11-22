03f68258acedc8006b9b545042238f53930ab087
paulinepar <paulineparmigiani@gmail.com>
2022-11-22 17:27:39 +0100

    print listint

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -4,9 +4,9 @@
 #include "lists.h"
 
 /**
- * print_list - print list
+ * print_dlistint- print list
  * @h: pointer const
- * Return: d
+ * Return: n
  */
 
 size_t print_dlistint(const dlistint_t *h)
