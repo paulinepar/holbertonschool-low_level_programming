73c3a2903876dab1f398999912a7fdaeafe2e050
paulinepar <paulineparmigiani@gmail.com>
2022-11-22 10:19:30 +0100

    create file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -21,6 +21,6 @@ int create_file(const char *filename, char *text_content)
 
 		close(fd);
 
-		write(fd, text_content, strlen(text_content));
+		write(STDOUT_FILENO, text_content, strlen(text_content));
 		return (1);
 }
