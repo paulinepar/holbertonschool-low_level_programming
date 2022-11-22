234be52b71da14bc4db83ff57cd4303f93edab0c
paulinepar <paulineparmigiani@gmail.com>
2022-11-22 17:38:13 +0100

    returns number

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint_len - print list
+ * @h: pointer const
+ * Return: n
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int n = 0;
+
+	for (n = 0; h != NULL; n++)
+		h = h->next;
+
+	return (n);
+}
