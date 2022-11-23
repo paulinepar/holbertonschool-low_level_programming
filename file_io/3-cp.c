4dc2f62492ad1657d5c798b388b8ee688aff2a1b
paulinepar <paulineparmigiani@gmail.com>
2022-11-23 13:04:39 +0100

    cp file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to s%\n", argv[1]);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to %s\n", argv[1]);
 		exit(98);
 	}
 	fd2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
