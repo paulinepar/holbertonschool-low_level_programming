b5a4a5c476277c1276bb01a1c2138ef24e96d483
paulinepar <paulineparmigiani@gmail.com>
2022-11-23 12:01:08 +0100

    cp file

---
+++ file_io/3-cp.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *  append_text_to_file - add text at the end
+ * @filename: pointer of char
+ * @text_content: pointer of char
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int fd, fd2, size, wr;
+	char buffer[1024];
+
+	if (argc != 3)
+		exit(97);
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to s%\n", argv[1]);
+		exit(98);
+	}
+	fd2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	while (size > 0)
+		//le buffer recupere les données de fd//
+	size = read(fd, buffer, 1024);
+	if (size == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	wr = write(fd2, buffer, size);
+	if (wr == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	if (close(fd2) == -1)
+	{
+		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd2);
+	}
+	if (close(fd2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd2);
+		exit(100);
+	}
+	return (0);
+}
