6ab71e28ba8100a03ff1ce28d1845aff504b1958
paulinepar <paulineparmigiani@gmail.com>
2022-11-23 13:51:24 +0100

    add node list

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -14,9 +14,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *p;
 
-	if (head == NULL)
-		return (NULL);
-
 	p = malloc(sizeof(dlistint_t));
 	if (p == NULL)
 		return (NULL);
