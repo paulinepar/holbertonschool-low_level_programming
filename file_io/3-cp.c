2769c2f47c95bd6a3affdf9de197a4ab9bc6eeb3
paulinepar <paulineparmigiani@gmail.com>
2022-11-23 13:29:24 +0100

    cp file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -18,16 +18,14 @@ int main(int argc, char *argv[])
 	}
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	{ dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	while (size > 0)
 	size = read(fd, buffer, 1024);
 	if (size == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	{ dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	wr = write(fd2, buffer, size);
