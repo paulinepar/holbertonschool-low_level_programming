77f4ff06404e90ace9c7444baaa7595797efcb1e
paulinepar <paulineparmigiani@gmail.com>
2022-11-25 13:55:48 +0100

    creat hash table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -17,9 +17,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	new->array = malloc(sizeof(hash_node_t) * size);
-	if (new->array == NULL)
-		return (NULL);
-
 
 	new->size = size;
 	return (new);
